8c27d37351917b1cd23a46ea096dcd93e353ac93
Filip Hodun <filiphodun@gmail.com>
2021-08-06 20:30:32 +0200

    add recognition of the ongoing broadcast

--- utils.go
+++ utils.go
@@ -8,12 +8,24 @@ import (
 
 // Find next broadcast start and end date
 func FindNextBroadcast(timeNow time.Time) (time.Time, time.Time) {
-	auditionStart := time.Date(timeNow.Year(), timeNow.Month(), timeNow.Day(), 23, 59, 0, 0, timeNow.Location())
-	for auditionStart.Weekday() != time.Friday {
-		auditionStart = auditionStart.AddDate(0, 0, 1)
+	var (
+		auditionStart time.Time
+		auditionEnd   time.Time
+	)
+
+	if (timeNow.Weekday() == time.Saturday) && (timeNow.Hour() < 6) {
+		auditionStart = timeNow
+	} else {
+		auditionStart = time.Date(timeNow.Year(), timeNow.Month(), timeNow.Day(), 23, 59, 0, 0, timeNow.Location())
+		for auditionStart.Weekday() != time.Friday {
+			auditionStart = auditionStart.AddDate(0, 0, 1)
+		}
 	}
 
-	auditionEnd := time.Date(auditionStart.Year(), auditionStart.Month(), auditionStart.Day()+1, 6, 0, 0, 0, auditionStart.Location())
+	auditionEnd = time.Date(auditionStart.Year(), auditionStart.Month(), auditionStart.Day(), 6, 0, 0, 0, auditionStart.Location())
+	if auditionEnd.Weekday() == time.Friday {
+		auditionEnd = auditionEnd.AddDate(0, 0, 1)
+	}
 
 	return auditionStart, auditionEnd
 }
